5fc1e831c205e9154c8811553192a5513aa46a65
Stefan3201 <151079943+Stefan3201@users.noreply.github.com>
2023-11-16 16:02:35 -0500

    Update load_data.py
    added machine_vendor_list.py

--- load_data.py
+++ load_data.py
@@ -11,16 +11,16 @@ __team__ = "T38"
 # Place your machine_vendor_list function after this line
 def machine_vendor_list(filename: str, param: str) -> list[dict]:
     """
-    Return a list of machines that belong to a specifc vendor. Return an empty list if the vendor does not exist.
+    Return a list of machines that belong to a specific vendor. Return an empty list if the vendor does not exist.
 
     Preconditions: None
 
-    >>> machine_vendor_list(machine.csv, amdahl)
-    [{'Model': '32/60', 'MYCT': '125', 'MMIN': '256', 'MMAX': '6000', 'CACH': '256', 'PRP': '198', 'ERP': '199'}]
+    >>> machine_vendor_list(machine.csv, adviser)
+    [{'Model': '32/60', 'MYCT': 125, 'MMIN': 256, 'MMAX': 6000, 'CACH': 256, 'PRP': 198, 'ERP': 199}]
     >>> machine_vendor_list(machine.csv, apollo)
-    [{'Model': 'dn320', 'MYCT': '400', 'MMIN': '1000', 'MMAX': '3000', 'CACH': '0', 'PRP': '38', 'ERP': '23'}, {'Model': 'dn420', 'MYCT': '400', 'MMIN': '512', 'MMAX': '3500', 'CACH': '4', 'PRP': '40', 'ERP': '24'}]
+    [{'Model': 'dn320', 'MYCT': 400, 'MMIN': 1000, 'MMAX': 3000, 'CACH': 0, 'PRP': 38, 'ERP': 23}, {'Model': 'dn420', 'MYCT': 400, 'MMIN': 512, 'MMAX': 3500, 'CACH': 4, 'PRP': 40, 'ERP': 24}]
     >>> machine_vendor_list(machine.csv, bti)
-    [{'Model': '5000', 'MYCT': '350', 'MMIN': '64', 'MMAX': '64', 'CACH': '0', 'PRP': '10', 'ERP': '15'}, {'Model': '8000', 'MYCT': '200', 'MMIN': '512', 'MMAX': '16000', 'CACH': '0', 'PRP': '35', 'ERP': '64'}]
+    [{'Model': '5000', 'MYCT': 350, 'MMIN': 64, 'MMAX': 64, 'CACH': 0, 'PRP': 10, 'ERP': 15}, {'Model': '8000', 'MYCT': 200, 'MMIN': 512, 'MMAX': 16000, 'CACH': 0, 'PRP': 35, 'ERP': 64}]
     """
     infile = open(filename, "r")
     header = infile.readline().strip().split(",")
@@ -32,9 +32,17 @@ def machine_vendor_list(filename: str, param: str) -> list[dict]:
             for i in range(1, len(header)):
                 data[header[i]] = line_lst[i]
 
+                if i != 1:
+                    try:
+                        data[header[i]] = int(line_lst[i])
+                    except:
+                        data[header[i]] = line_lst[i]
+
             result.append(data)
     infile.close()
     return result
+
+#Do not include a main script
 
 #==========================================#
 # Place your machine_model_list function after this line
