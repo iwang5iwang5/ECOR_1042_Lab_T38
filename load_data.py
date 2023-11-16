540b9a8ace8e1a5d9d4e96e3eba335d503be936b
iwang5iwang5 <63758532+iwang5iwang5@users.noreply.github.com>
2023-11-16 14:41:14 -0500

    Add files via upload

---
+++ load_data.py
@@ -0,0 +1,40 @@
+# ECOR 1042 Lab 3 - Template
+
+
+# Update "" to list all students contributing to the team work
+__author__ = ""
+
+# Update "" with your team (e.g. T102)
+__team__ = "Sample"
+
+#==========================================#
+# Place your machine_vendor_list function after this line
+
+
+
+#==========================================#
+# Place your machine_model_list function after this line
+
+
+#==========================================#
+# Place your machine_cache_list function after this line
+
+
+
+#==========================================#
+# Place your machine_prp_list function after this line
+
+
+
+#==========================================#
+# Place your load_data function after this line
+
+
+#==========================================#
+# Place your add_average_main_memory function after this line
+
+
+
+# Do NOT include a main script in your submission
+
+
---
+++ machine_model_list.py
@@ -0,0 +1,80 @@
+# ECOR 1042 Lab 3 - Template
+
+
+# Update 
+__author__ = "Ivan Wang"
+__student_number__ = "101298751"
+__team__ = "T038"
+
+#==========================================#
+# Place your machine_model_list function after this line
+def machine_model_list(file_name: str, model: str) -> list[dict]:
+    """Return list of machine dictionaries of the specified model. The dictionaries 
+    contain all given information execpt the model with the header as the key given 
+    the file_name containing the path to a csv file and the model.
+    
+    Preconditions: len(file_name) > 0, len(model) > 0
+
+    >>>machine_model_list("machine.csv", '32/60')
+    [{'Vendor': 'adviser', 'MYCT': 125, 'MMIN': 256, 'MMAX': 6000, 'CACH': 256, 'PRP': 198, 'ERP': 199}]
+    >>>machine_model_list("machine.csv", 'non existent model')
+    []
+    >>>machine_model_list("machine.csv", 'dn420')
+    [{'Vendor': 'apollo', 'MYCT': 400, 'MMIN': 512, 'MMAX': 3500, 'CACH': 4, 'PRP': 40, 'ERP': 24}]
+    """
+    #create list
+    machine_list = []
+    
+    #open file
+    with open(file_name, "r") as file:
+        
+        #get the header
+        headers = file.readline().split(",")
+
+        #remove white space from header and check for model
+        for i in range(len(headers)):
+            headers[i] = headers[i].strip()
+
+        #read the first line
+        line = file.readline()
+        
+        #loop for each line of the file.
+        while line != '':
+        
+            #make sure line is not empty
+            if line != '\n':
+                
+                #split line
+                line = line.split(",")
+
+                #create temperary dictionary
+                temp_dict = {}
+
+                #check if the model are the same as the specified model
+                if model == line[1]:
+
+                    #loop for each header
+                    for i in range(len(headers)):
+
+                        #remove white space
+                        line[i] = line[i].strip()
+                        
+                        #make sure
+                        if i != 1:
+
+                            
+                            try:#try to int casting the line
+                                temp_dict[headers[i]] = int(line[i])
+                            except:#if not store value as str
+                                temp_dict[headers[i]] = line[i]
+
+                    #add new dictionary to list
+                    machine_list += [temp_dict]
+
+            #read next line
+            line = file.readline()
+            
+    #return
+    return machine_list
+
+#Do not include a main script
