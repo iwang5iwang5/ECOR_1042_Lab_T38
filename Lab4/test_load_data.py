da7fee0697b8c3e0ec980db35161c3103326d1ce
Snyvie7 <151079538+Snyvie7@users.noreply.github.com>
2023-11-23 15:00:41 -0500

    test load data has been given test 1

--- Lab4/test_load_data.py
+++ Lab4/test_load_data.py
@@ -16,6 +16,64 @@ __team__ = "T-038"
 
 # Place test_return_list function here 
 
+def test_return_list():
+    # Complete the function with your test cases
+    vendor_test1 = load_data.machine_vendor_list("machine-test.csv", "basf")
+    check.equal(type(vendor_test1), list)
+    vendor_test2 = load_data.machine_vendor_list("machine-test.csv", "reek")
+    check.equal(type(vendor_test2), list)
+    vendor_test3 = load_data.machine_vendor_list(
+        "machine-test.csv", "apollo")
+    check.equal(type(vendor_test3), list)
+
+    model_test1 = load_data.machine_model_list("machine-test.csv", 5000)
+    check.equal(type(model_test1), list)
+    model_test2 = load_data.machine_model_list(
+        "machine-test.csv", "580-5840")
+    check.equal(type(model_test2), list)
+    model_test3 = load_data.machine_model_list("machine-test.csv", "b0000")
+    check.equal(type(model_test3), list)
+
+    cache_test1 = load_data.machine_cache_list("machine-test.csv", 32)
+    check.equal(type(cache_test1), list)
+    cache_test2 = load_data.machine_cache_list("machine-test.csv", 200)
+    check.equal(type(cache_test2), list)
+    cache_test3 = load_data.machine_cache_list("machine-test.csv", 142)
+    check.equal(type(cache_test3), list)
+
+    prp_test1 = load_data.machine_prp_list("machine-test.csv", 1200)
+    check.equal(type(prp_test1), list)
+    prp_test2 = load_data.machine_prp_list("machine-test.csv", 1144)
+    check.equal(type(prp_test2), list)
+    prp_test3 = load_data.machine_prp_list("machine-test.csv", 172)
+    check.equal(type(prp_test3), list)
+
+    load_data_test1 = load_data.load_data("machine-test.csv", ("CACH", 32))
+    check.equal(type(load_data_test1), list)
+    load_data_test2 = load_data.load_data("machine-test.csv", ("PRP", 269))
+    check.equal(type(load_data_test2), list)
+    load_data_test3 = load_data.load_data(
+        "machine-test.csv", ("MODEL", "Jul-65"))
+    check.equal(type(load_data_test3), list)
+    load_data_test4 = load_data.load_data(
+        "machine-test.csv", ("VENDOR", "amdahl"))
+    check.equal(type(load_data_test4), list)
+    load_data_test5 = load_data.load_data("machine-test.csv", ("All", 2000))
+    check.equal(type(load_data_test5), list)
+    load_data_test6 = load_data.load_data("machine-test.csv", ("MMIN", 512))
+    check.equal(type(load_data_test6), list)
+
+    add_average_main_memory_test1 = load_data.add_average_main_memory(
+        load_data.machine_vendor_list("machine-test.csv", "basf"))
+    check.equal(type(add_average_main_memory_test1), list)
+    add_average_main_memory_test2 = load_data.add_average_main_memory(
+        load_data.machine_model_list("machine-test.csv", "apollo"))
+    check.equal(type(add_average_main_memory_test2), list)
+    add_average_main_memory_test3 = load_data.add_average_main_memory(
+        load_data.machine_prp_list("machine-test.csv", 2000))
+    check.equal(type(add_average_main_memory_test3), list)
+
+    check.summary()
 
 # Place test_return_list_correct_lenght function here
 def test_return_list_correct_lenght() -> None:
