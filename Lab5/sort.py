8f2c67fc6de60849d9112986be68520a592f7b0f
iwang5iwang5 <63758532+iwang5iwang5@users.noreply.github.com>
2023-11-27 23:02:30 -0500

    Created lab 5

---
+++ Lab5/sort.py
@@ -0,0 +1,30 @@
+# ECOR 1042 Lab 5 - Team submission
+# Remember to include docstring and type annotations for your functions
+
+# Update "" to list all students contributing to the team work
+__author__ = ""
+
+# Update "" with your team (e.g. T102)
+__team__ = ""
+
+#==========================================#
+# Place your sort_cache_bubble function after this line
+
+
+#==========================================#
+# Place your sort_prp_selection function after this line
+
+
+#==========================================#
+# Place your sort_m_avg_insertion function after this line
+
+
+#==========================================#
+# Place your sort_myct_bubble function after this line
+
+
+#==========================================#
+# Place your sort function after this line
+
+
+# Do NOT include a main script in your submission
