0def529d8277efa12963b741d975606b4d687ef2
iwang5iwang5 <63758532+iwang5iwang5@users.noreply.github.com>
2023-11-30 15:48:38 -0500

    module 2 edit

--- Lab6/batch_UI.py
+++ Lab6/batch_UI.py
@@ -16,6 +16,35 @@ import histogram
 import load_data
 import sort
 
+def load_data_command(e:list):
+    pass
+def curve_fit_command(e:list):
+    pass
+def histogram_command(e:list):
+    pass
+def sort_command(e:list):
+    pass
 
+# filename = input("Please enter the name of the file where your commands are stored: ")
+filename = "batch.txt"
+with open(filename, "r") as batch:
+    commands = batch.read().strip().split("\n")
+    for i in range(len(commands)):
+        commands[i] = commands[i].strip().split(";")
+    print(commands)
 
+    not_exit = True
+    i = 0
 
+    while i < len(commands) and not_exit:
+        if commands[i][0] == "L":
+            load_data_command(commands[i])
+        elif commands[i][0] == "S":
+            sort_command(commands[i])
+        elif commands[i][0] == "C":
+            curve_fit_command(commands[i])
+        elif commands[i][0] == "H":
+            histogram_command(commands[i])
+        elif commands[i][0] == "E":
+            not_exit = False
+        i += 1
