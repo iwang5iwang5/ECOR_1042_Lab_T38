11f7d422b21fb977506bca3133cfb2c8144e6e63
Snyvie7 <151079538+Snyvie7@users.noreply.github.com>
2023-12-06 20:43:09 -0500

    e

--- Lab6/text_UI.py
+++ Lab6/text_UI.py
@@ -21,7 +21,7 @@ m_avg = load_data.add_average_main_memory
 from sort import sort
 
 # Import the histogram function from the histogram file
-from histogram import histogram
+import histogram
 
 # Import the curve_fit function from the curve_fit file
 from curve_fit import curve_fit
@@ -260,6 +260,6 @@ def histogram_of_data_command(data: list[dict], loaded_type: str) -> None:
         else:
             print("Invalid attribute.")
 
-    temp = histogram(data, histogram_attribute) # Vendor and Model not working ;-;
+    temp = histogram.histogram(data, histogram_attribute) # Vendor and Model not working ;-;, to fix we need to make sure that all names are of the same type (i.e. all strings)
 
 get_command()
